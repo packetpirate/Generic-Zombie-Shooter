d84212c2671085561e903be429c4f25bbb21a185
packetpirate <packetpirate@gmail.com>
2013-08-19 12:36:14 -0400

    Changed sprite sheet for zombie dog to a sprite that looks appropriate for a top-down perspective.

--- GenericZombieShooter/src/genericzombieshooter/GZSFramework.java
+++ GenericZombieShooter/src/genericzombieshooter/GZSFramework.java
@@ -361,7 +361,7 @@ public class GZSFramework {
             zombies.add(z_);
         } else if(type == Globals.ZOMBIE_DOG_TYPE) {
             // Fast Zombie Dog
-            Animation a_ = new Animation(Images.ZOMBIE_DOG, 48, 48, 4, (int)p_.x, (int)p_.y, 100, 0, true);
+            Animation a_ = new Animation(Images.ZOMBIE_DOG, 50, 50, 4, (int)p_.x, (int)p_.y, 100, 0, true);
             Zombie z_ = new Zombie(p_, 100, 3, 3, 150, a_);
             zombies.add(z_);
         }
